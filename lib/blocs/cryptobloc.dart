e07a566f3326af8a30390ba9dbb8aae83912ec8a
Adnan Ali <transcend777ice@gmail.com>
2021-05-25 23:58:58 +0530

    accessed blocs on home screen

---
+++ lib/blocs/cryptobloc.dart
@@ -0,0 +1,21 @@
+import 'package:crypto_app/models/crytomodel.dart';
+import 'package:crypto_app/resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
+
+class CryptoBlock {
+  final _repository = Repository();
+  final _currencyfetcher = PublishSubject<List<CryptoModel>>();
+
+  Stream<List<CryptoModel>> get allcurrency => _currencyfetcher.stream;
+
+  fetchAllMovies() async {
+    List<CryptoModel> cryptomodel = await _repository.fetchallcurrencies();
+    _currencyfetcher.sink.add(cryptomodel);
+  }
+
+  dispose() {
+    _currencyfetcher.close();
+  }
+}
+
+final bloc = CryptoBlock();
--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:crypto_app/blocs/cryptobloc.dart';
+import 'package:crypto_app/models/crytomodel.dart';
 import 'package:crypto_app/resources/network_calls.dart';
 import 'package:crypto_app/resources/repository.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +19,24 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _repository.fetchallcurrencies();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Crypbit'),
+      ),
+      body: StreamBuilder(
+        stream: bloc.allcurrency,
+        builder: (context, AsyncSnapshot<List<CryptoModel>> snapshot) {
+          if (snapshot.hasData) {
+            return Container();
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 }
