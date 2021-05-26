3b93f86c202b8b489019e56ed49c39dcb13882be
Adnan Ali <transcend777ice@gmail.com>
2021-05-26 20:57:46 +0530

    formatted api data

--- lib/resources/network_calls.dart
+++ lib/resources/network_calls.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
+import 'package:intl/intl.dart';
+
 class CurrencyApiProvider {
   static final _key = '7e2b3169a3af6ec3e8d9dfa4dfcc213be76cb8fe';
 
@@ -19,8 +21,13 @@ class CurrencyApiProvider {
       }
       final body = json.decode(response.body) as List;
       result = body.map((e) => CryptoModel.fromJson(e)).toList();
+      /*result.forEach((e) {
+        print(NumberFormat.compact().format(double.parse(e.marketCap)));
+      });*/
       result.forEach((e) {
-        print(e.oneDayChange);
+        print(NumberFormat.currency(
+                decimalDigits: 3, symbol: '\$', locale: 'en_us')
+            .format(double.parse(e.price)));
       });
     } catch (e) {
       print(e.toString());
